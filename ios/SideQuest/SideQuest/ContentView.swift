94661d9340e6b33579478cf5f45357c4c84b1e0b
Oz789 <osvaldom5427@gmail.com>
2026-01-12 16:12:31 -0600

    iOS project scaffold

---
+++ ios/SideQuest/SideQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SideQuest
+//
+//  Created by Osvaldo Mosso on 1/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/SideQuest/SideQuest/SideQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideQuestApp.swift
+//  SideQuest
+//
+//  Created by Osvaldo Mosso on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SideQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
